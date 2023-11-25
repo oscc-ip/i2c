9db1d4132ba35b13ec52b35f6cde78277d7b569e
maksyuki <maksyuki@126.com>
2023-11-25 22:00:20 +0800

    feat: add i2c define file

--- rtl/apb4_i2c.sv
+++ rtl/apb4_i2c.sv
@@ -20,35 +20,20 @@
 // MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 // See the Mulan PSL v2 for more details.
 
-// verilog_format: off
-`define I2C_PSCR 4'b0000 //BASEADDR+0x00
-`define I2C_CTRL 4'b0001 //BASEADDR+0x04
-`define I2C_TXR  4'b0010 //BASEADDR+0x08
-`define I2C_RXR  4'b0011 //BASEADDR+0x0C
-`define I2C_CMD  4'b0100 //BASEADDR+0x10
-`define I2C_SR   4'b0101 //BASEADDR+0x14
-// verilog_format: on
+`include "i2c_define.sv"
 
 module apb4_i2c (
-    // verilog_format: off
     apb4_if.slave apb4,
-    // verilog_format: on
-    input logic  scl_i,
-    output logic scl_o,
-    output logic scl_dir_o,
-    input logic  sda_i,
-    output logic sda_o,
-    output logic sda_dir_o,
-    output logic irq_o
+    i2c_if.dut    i2c
 );
 
   logic [3:0] s_apb_addr;
   logic s_apb4_wr_hdshk, s_apb4_rd_hdshk;
-  logic [15:0] s_i2c_pscr_d, s_i2c_pscr_q;
-  logic [7:0] s_i2c_ctrl_d, s_i2c_ctrl_q;
-  logic [7:0] s_i2c_txr_d, s_i2c_txr_q;
-  logic [7:0] s_i2c_rxr;
-  logic [7:0] s_i2c_cmd_d, s_i2c_cmd_q;
+  logic [`I2C_PSCR_WIDTH-1:0] s_i2c_pscr_d, s_i2c_pscr_q;
+  logic [`I2C_CTRL_WIDTH-1:0] s_i2c_ctrl_d, s_i2c_ctrl_q;
+  logic [`I2C_TXR_WIDTH-1:0] s_i2c_txr_d, s_i2c_txr_q;
+  logic [`I2C_RXR_WIDTH-1:0] s_i2c_rxr;
+  logic [`I2C_CMD_WIDTH-1:0] s_i2c_cmd_d, s_i2c_cmd_q;
   logic [7:0] s_i2c_sr;
   logic s_i2c_done, s_i2c_en, s_i2c_ien, s_i2c_irxack;
   logic s_i2c_rxack_d, s_i2c_rxack_q;
@@ -58,38 +43,41 @@ module apb4_i2c (
   logic s_i2c_al_d, s_i2c_al_q;
   logic s_irq_d, s_irq_q;
 
-  assign s_apb_addr      = apb4.paddr[5:2];
+  assign s_apb_addr = apb4.paddr[5:2];
   assign s_apb4_wr_hdshk = apb4.psel && apb4.penable && apb4.pwrite;
   assign s_apb4_rd_hdshk = apb4.psel && apb4.penable && (~apb4.pwrite);
-  assign s_i2c_en        = s_i2c_ctrl_q[7];
-  assign s_i2c_ien       = s_i2c_ctrl_q[6];
-  assign s_i2c_sr[7]     = s_i2c_rxack_q;
-  assign s_i2c_sr[6]     = s_i2c_busy;
-  assign s_i2c_sr[5]     = s_i2c_al_q;
-  assign s_i2c_sr[4:2]   = 3'b0;
-  assign s_i2c_sr[1]     = s_i2c_tip_q;
-  assign s_i2c_sr[0]     = s_i2c_irq_q;
-  assign irq_o           = s_irq_q;
+  assign apb4.pready = 1'b1;
+  assign apb4.pslverr = 1'b0;
+
+  assign s_i2c_en = s_i2c_ctrl_q[7];
+  assign s_i2c_ien = s_i2c_ctrl_q[6];
+  assign s_i2c_sr[7] = s_i2c_rxack_q;
+  assign s_i2c_sr[6] = s_i2c_busy;
+  assign s_i2c_sr[5] = s_i2c_al_q;
+  assign s_i2c_sr[4:2] = 3'b0;
+  assign s_i2c_sr[1] = s_i2c_tip_q;
+  assign s_i2c_sr[0] = s_i2c_irq_q;
+  assign i2c.irq_o = s_irq_q;
 
 
-  dffr #(16) u_i2c_pscr_dffr (
+  assign s_i2c_pscr_d = s_apb4_wr_hdshk && (s_apb_addr == `I2C_PSCR) ? apb4.pwdata[`I2C_PSCR_WIDTH-1:0] : s_i2c_pscr_q;
+  dffrc #(`I2C_PSCR_WIDTH, `I2C_PSCR_MIN_VAL) u_i2c_pscr_dffrc (
       apb4.hclk,
       apb4.hresetn,
       s_i2c_pscr_d,
       s_i2c_pscr_q
   );
 
-  assign s_i2c_pscr_d = s_apb4_wr_hdshk && (s_apb_addr == `I2C_PSCR) ? apb4.pwdata[15:0] : s_i2c_pscr_q;
-  assign s_i2c_ctrl_d = s_apb4_wr_hdshk && (s_apb_addr == `I2C_CTRL) ? apb4.pwdata[7:0] : s_i2c_ctrl_q;
-  dffr #(8) u_i2c_ctrl_dffr (
+  assign s_i2c_ctrl_d = s_apb4_wr_hdshk && (s_apb_addr == `I2C_CTRL) ? apb4.pwdata[`I2C_CTRL_WIDTH-1:0] : s_i2c_ctrl_q;
+  dffr #(`I2C_CTRL_WIDTH) u_i2c_ctrl_dffr (
       apb4.hclk,
       apb4.hresetn,
       s_i2c_ctrl_d,
       s_i2c_ctrl_q
   );
 
-  assign s_i2c_txr_d = s_apb4_wr_hdshk && (s_apb_addr == `I2C_TXR) ? apb4.pwdata[7:0] : s_i2c_txr_q;
-  dffr #(8) u_i2c_txr_dffr (
+  assign s_i2c_txr_d = s_apb4_wr_hdshk && (s_apb_addr == `I2C_TXR) ? apb4.pwdata[`I2C_TXR_WIDTH-1:0] : s_i2c_txr_q;
+  dffr #(`I2C_TXR_WIDTH) u_i2c_txr_dffr (
       apb4.hclk,
       apb4.hresetn,
       s_i2c_txr_d,
@@ -102,13 +90,13 @@ module apb4_i2c (
     s_i2c_cmd_d[2:0] = 3'b0;
 
     if (s_apb4_wr_hdshk && s_apb_addr == `I2C_CMD && s_i2c_en) begin
-      s_i2c_cmd_d = apb4.pwdata[7:0];
+      s_i2c_cmd_d = apb4.pwdata[`I2C_CMD_WIDTH-1:0];
     end else if (s_i2c_done | s_i2c_al) begin
       s_i2c_cmd_d[7:4] = 4'b0;
     end
   end
 
-  dffr #(8) u_i2c_cmd_dffr (
+  dffr #(`I2C_CMD_WIDTH) u_i2c_cmd_dffr (
       apb4.hclk,
       apb4.hresetn,
       s_i2c_cmd_d,
@@ -159,12 +147,13 @@ module apb4_i2c (
     apb4.prdata = '0;
     if (s_apb4_rd_hdshk) begin
       unique case (s_apb_addr)
-        `I2C_PSCR: apb4.prdata = {16'b0, s_i2c_pscr_q};
-        `I2C_CTRL: apb4.prdata = {24'b0, s_i2c_ctrl_q};
-        `I2C_TXR:  apb4.prdata = {24'b0, s_i2c_txr_q};
-        `I2C_RXR:  apb4.prdata = {24'b0, s_i2c_rxr_q};
-        `I2C_CMD:  apb4.prdata = {24'b0, s_i2c_cmd_q};
-        `I2C_SR:   apb4.prdata = {24'b0, s_i2c_sr};
+        `I2C_PSCR: apb4.prdata[`I2C_PSCR_WIDTH-1:0] = s_i2c_pscr_q;
+        `I2C_CTRL: apb4.prdata[`I2C_CTRL_WIDTH-1:0] = s_i2c_ctrl_q;
+        `I2C_TXR:  apb4.prdata[`I2C_TXR_WIDTH-1:0] = s_i2c_txr_q;
+        `I2C_RXR:  apb4.prdata[`I2C_RXR_WIDTH-1:0] = s_i2c_rxr_q;
+        `I2C_CMD:  apb4.prdata[`I2C_CMD_WIDTH-1:0] = s_i2c_cmd_q;
+        `I2C_SR:   apb4.prdata[`I2C_SR_WIDTH-1:0] = s_i2c_sr;
+        default:   apb4.prdata = '0;
       endcase
     end
   end
@@ -185,15 +174,12 @@ module apb4_i2c (
       .dat_o     (s_i2c_rxr),
       .i2c_busy_o(s_i2c_busy),
       .i2c_al_o  (s_i2c_al),
-      .scl_i,
-      .scl_o,
-      .scl_dir_o,
-      .sda_i,
-      .sda_o,
-      .sda_dir_o
+      .scl_i     (i2c.scl_i),
+      .scl_o     (i2c.scl_o),
+      .scl_dir_o (i2c.scl_dir_o),
+      .sda_i     (i2c.sda_i),
+      .sda_o     (i2c.sda_o),
+      .sda_dir_o (i2c.sda_dir_o)
   );
-
-  assign apb4.pready  = 1'b1;
-  assign apb4.pslverr = 1'b0;
 
 endmodule
---
+++ rtl/i2c_define.sv
@@ -0,0 +1,102 @@
+// Copyright (c) 2023 Beijing Institute of Open Source Chip
+// i2c is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//             http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+`ifndef INC_I2C_DEF_SV
+`define INC_I2C_DEF_SV
+
+/* register mapping
+ * I2C_CTRL:
+ * BITS:   | 31:8 | 7  | 6   | 5:0  |
+ * FIELDS: | RES  | EN | IEN | RES  |
+ * PERMS:  | NONE | RW | RW  | NONE |
+ * -----------------------------------------------------------
+ * I2C_PSCR:
+ * BITS:   | 31:16 | 15:0 |
+ * FIELDS: | RES   | PSCR |
+ * PERMS:  | NONE  | RW   |
+ * -----------------------------------------------------------
+ * I2C_TXR:
+ * BITS:   | 31:8  | 7:1  | 0  |
+ * FIELDS: | RES   | DATA | MD |
+ * PERMS:  | NONE  | W    | W  |
+ * -----------------------------------------------------------
+ * I2C_RXR:
+ * BITS:   | 31:8 | 7:0  |
+ * FIELDS: | RES  | DATA |
+ * PERMS:  | NONE | R    |
+ * -----------------------------------------------------------
+ * I2C_CMD:
+ * BITS:   | 31:8 | 7   | 6   | 5  | 4   | 3   | 2:1  | 0    |
+ * FIELDS: | RES  | STA | STO | RD | WR  | ACK | RES  | IACK |
+ * PERMS:  | NONE | W   | W   | W  | W   | W   | NONE | W    |
+ * -----------------------------------------------------------
+ * I2C_SR:
+ * BITS:   | 31:8  | 7   | 6   | 5  | 4:2 | 1   | 0  |
+ * FIELDS: | RES   | RXK | BSY | AL | RXK | TIP | IF |
+ * PERMS:  | NONE  | R   | R   | R  | R   | R   | R  |
+ * -----------------------------------------------------------
+*/
+
+// verilog_format: off
+`define I2C_CTRL 4'b0000 // BASEADDR + 0x00
+`define I2C_PSCR 4'b0001 // BASEADDR + 0x04
+`define I2C_TXR  4'b0010 // BASEADDR + 0x08
+`define I2C_RXR  4'b0011 // BASEADDR + 0x0C
+`define I2C_CMD  4'b0100 // BASEADDR + 0x10
+`define I2C_SR   4'b0101 // BASEADDR + 0x14
+
+`define I2C_CTRL_ADDR {26'b0, `I2C_CTRL, 2'b00}
+`define I2C_PSCR_ADDR {26'b0, `I2C_PSCR, 2'b00}
+`define I2C_TXR_ADDR  {26'b0, `I2C_TXR , 2'b00}
+`define I2C_RXR_ADDR  {26'b0, `I2C_RXR , 2'b00}
+`define I2C_CMD_ADDR  {26'b0, `I2C_CMD , 2'b00}
+`define I2C_SR_ADDR   {26'b0, `I2C_SR  , 2'b00}
+
+`define I2C_CTRL_WIDTH 8
+`define I2C_PSCR_WIDTH 16
+`define I2C_TXR_WIDTH  8
+`define I2C_RXR_WIDTH  8
+`define I2C_CMD_WIDTH  8
+`define I2C_SR_WIDTH   8
+
+`define I2C_PSCR_MIN_VAL  {{(`I2C_PSCR_WIDTH-2){1'b0}}, 2'd2}
+// verilog_format: on
+
+interface i2c_if ();
+  logic scl_i;
+  logic scl_o;
+  logic scl_dir_o;
+  logic sda_i;
+  logic sda_o;
+  logic sda_dir_o;
+  logic irq_o;
+
+  modport dut(
+      input scl_i,
+      output scl_o,
+      output scl_dir_o,
+      input sda_i,
+      output sda_o,
+      output sda_dir_o,
+      output irq_o
+  );
+
+  modport tb(
+      output scl_i,
+      input scl_o,
+      input scl_dir_o,
+      output sda_i,
+      input sda_o,
+      input sda_dir_o,
+      input irq_o
+  );
+endinterface
+
+`endif
