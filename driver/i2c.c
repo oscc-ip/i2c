3987e2c16b53c1d40739e328788128835a687659
maksyuki <maksyuki@126.com>
2024-08-07 11:13:39 +0800

    feat: format the i2c driver impl

--- driver/i2c.c
+++ driver/i2c.c
@@ -2,13 +2,13 @@
 #include <klib.h>
 #include <klib-macros.h>
 
-#define I2C_BASE_ADDR 0x10004000
-#define I2C_REG_CTRL *((volatile uint32_t *)(I2C_BASE_ADDR))
-#define I2C_REG_PSCR *((volatile uint32_t *)(I2C_BASE_ADDR + 4))
-#define I2C_REG_TXR  *((volatile uint32_t *)(I2C_BASE_ADDR + 8))
-#define I2C_REG_RXR  *((volatile uint32_t *)(I2C_BASE_ADDR + 12))
-#define I2C_REG_CMD  *((volatile uint32_t *)(I2C_BASE_ADDR + 16))
-#define I2C_REG_SR   *((volatile uint32_t *)(I2C_BASE_ADDR + 20))
+#define I2C_BASE_ADDR        0x10004000
+#define I2C_REG_CTRL         *((volatile uint32_t *)(I2C_BASE_ADDR))
+#define I2C_REG_PSCR         *((volatile uint32_t *)(I2C_BASE_ADDR + 4))
+#define I2C_REG_TXR          *((volatile uint32_t *)(I2C_BASE_ADDR + 8))
+#define I2C_REG_RXR          *((volatile uint32_t *)(I2C_BASE_ADDR + 12))
+#define I2C_REG_CMD          *((volatile uint32_t *)(I2C_BASE_ADDR + 16))
+#define I2C_REG_SR           *((volatile uint32_t *)(I2C_BASE_ADDR + 20))
 
 #define I2C_TEST_START       ((uint32_t)0x80)
 #define I2C_TEST_STOP        ((uint32_t)0x40)
@@ -25,11 +25,13 @@
 #define I2C_STATUS_TIP       ((uint32_t)0x02) // (1 << 1)
 #define I2C_STATUS_IF        ((uint32_t)0x01) // (1 << 0)
 
-#define TEST_NUM 16
-#define AT24C64_SLV_ADDR 0xA0
-#define AT24C64_SLV_ADDR 0xA0
+#define I2C_DEV_ADDR_16BIT   0
+#define I2C_DEV_ADDR_8BIT    1
 
-// uint32_t slv_addr;
+#define TEST_NUM             20
+#define AT24C64_SLV_ADDR     0xA0
+#define PCF8563B_SLV_ADDR    0xA2
+
 
 void i2c_config() {
     I2C_REG_CTRL = (uint32_t)0;
@@ -62,7 +64,7 @@ void i2c_rd_start(uint32_t slv_addr) {
     }while (!i2c_get_ack());
 }
 
-void i2c_write(uint32_t val) {
+void i2c_write(uint8_t val) {
     I2C_REG_TXR = val;
     I2C_REG_CMD = I2C_TEST_WRITE;
     if (!i2c_get_ack()) putstr("[i2c write]no ack recv\n");
@@ -70,7 +72,7 @@ void i2c_write(uint32_t val) {
 
 uint32_t i2c_read(uint32_t cmd) {
     I2C_REG_CMD = cmd;
-    if (!i2c_get_ack()) putstr("[i2c read]no ack recv from slv 6\n");
+    if (!i2c_get_ack()) putstr("[i2c read]no ack recv\n");
     return I2C_REG_RXR;
 }
 
@@ -79,45 +81,75 @@ void i2c_stop() {
     while(i2c_busy());
 }
 
-void AT24C64_wr_setup() {
-    i2c_wr_start(AT24C64_SLV_ADDR);
-    i2c_write((uint32_t)0); // send the high data word addr
-    i2c_write((uint32_t)0); // send the low data word addr
-}
-
-void AT24C64_rd_setup() {
-    i2c_rd_start(AT24C64_SLV_ADDR);
-    i2c_write((uint32_t)0); // send the high data word addr
-    i2c_write((uint32_t)0); // send the low data word addr
+void i2c_wr_nbyte(uint8_t slv_addr, uint16_t reg_addr, uint8_t type, uint8_t num, uint8_t *data) {
+    i2c_wr_start(slv_addr);
+    if(type == I2C_DEV_ADDR_16BIT) {
+        i2c_write((uint8_t)((reg_addr >> 8) & 0xFF));
+        i2c_write((uint8_t)(reg_addr & 0xFF));
+    } else {
+        i2c_write((uint8_t)reg_addr);
+    }
+    for(int i = 0; i < num; ++i) {
+        i2c_write(*data);
+        ++data;
+    }
     i2c_stop();
 }
+
+void i2c_rd_nbyte(uint8_t slv_addr, uint16_t reg_addr, uint8_t type, uint8_t num, uint8_t *data) {
+    i2c_rd_start(slv_addr);
+    if(type == I2C_DEV_ADDR_16BIT) {
+        i2c_write((uint8_t)((reg_addr >> 8) & 0xFF));
+        i2c_write((uint8_t)(reg_addr & 0xFF));
+    } else {
+        i2c_write((uint8_t)reg_addr);
+    }
+    i2c_stop();
+
+    i2c_wr_start(slv_addr + 1);
+    for (int i = 0; i < num; ++i) {
+        if (i == num - 1) data[i] = i2c_read(I2C_TEST_STOP_READ);
+        else data[i] = i2c_read(I2C_TEST_READ);
+    }
+}
+
+uint8_t PCF8563B_bin2bcd(uint8_t val) {
+    uint8_t bcdhigh = 0;
+    while (val >= 10) {
+        ++bcdhigh;
+        val -= 10;
+    }
+    return ((uint8_t)(bcdhigh << 4) | val);
+}
+
+uint8_t PCF8563B_bcd2bin(uint8_t val) {
+    uint8_t tmp = 0;
+    tmp = ((uint8_t)(val & (uint8_t)0xF0) >> (uint8_t)0x04) * 10;
+    return (tmp + (val & (uint8_t)0x0F));
+}
+
 
 int main(){
     putstr("i2c test\n");
     i2c_config();
-    // write
-    AT24C64_wr_setup();
+    // wr AT24C64
+    uint8_t ref_data[TEST_NUM], rd_data[TEST_NUM];
+    for(int i = 0; i < TEST_NUM; ++i) ref_data[i] = i;
+
+    i2c_wr_nbyte(AT24C64_SLV_ADDR, (uint16_t)0, I2C_DEV_ADDR_16BIT, TEST_NUM, ref_data);
+    putstr("AT24C64 wr page done\n");
+    i2c_rd_nbyte(AT24C64_SLV_ADDR, (uint16_t)0, I2C_DEV_ADDR_16BIT, TEST_NUM, rd_data);
+
     for(int i = 0; i < TEST_NUM; ++i) {
-        i2c_write((uint32_t)i);
+        printf("recv: %d expt: %d\n", rd_data[i], i);
+        if (rd_data[i] != i) putstr("test fail\n");
     }
-    i2c_stop();
-    putstr("AT24C64 tx done\n");
+    putstr("AT24C64 rd page done\n");
 
     // read
-    AT24C64_rd_setup();
-    i2c_wr_start(AT24C64_SLV_ADDR + 1);
-    uint32_t recv_val;
-    for (int i = 0; i < TEST_NUM; ++i) {
-        if (i == TEST_NUM - 1) recv_val = i2c_read(I2C_TEST_STOP_READ);
-        else recv_val = i2c_read(I2C_TEST_READ);
 
-        printf("recv: %d expt: %d\n", recv_val, i);
-        if (recv_val != i) putstr("test fail\n");
-    }
-    putstr("AT24C64 rx done\n");
-
-    putstr("pcf8563 wr done\n");
-    putstr("pcf8563 rd done\n");
+    putstr("PCF8563B wr done\n");
+    putstr("PCF8563B rd done\n");
 
     putstr("test done\n");
     return 0;
